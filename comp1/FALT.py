fa460436cda3bf6bd97ec650a1c95a786dd9a2ba
Cameron <cameronbriar@mail.fresnostate.edu>
2012-08-03 01:01:46 -0700

    added custom function

--- comp1/FALT.py
+++ comp1/FALT.py
@@ -36,10 +36,10 @@ dictionaryFreqs = "words_freq_m"
 symbols = {'crosshatch': [u'\u25a9', '&#9641;'], 'snowman': [u'\u2603', '&#9731;'], 'at': [u'@', '&#64;'], 'female': [u'\u2640', '&#9792;'], 'scissors': [u'\u2704', '&#9988;'], 'airplane': [u'\u2708', '&#9992;'], 'cloud': [u'\u2601', '&#9729;'], 'fourpoint': [u'\u2726', '&#10022;'], 'flower': [u'\u273f', '&#10047;'], 'phonemic': [u'\u260e', '&#9742;'], 'sun': [u'\u2742', '&#10050;'], 'peace': [u'\u270c', '&#9996;'], 'blackstar': [u'\u2738', '&#10040;'], 'elevator': [u'\u27e0', '&#10208;'], 'chess': [u'\u265a', '&#9818;'], 'smile': [u'\u263a', '&#9786;'], 'circle': [u'\u29bf', '&#10687;'], 'wheel': [u'\u2638', '&#9784;'], 'pencil': [u'\u270f', '&#9998;'], 'coffee': [u'\u2668', '&#9832;'], 'umbrella': [u'\u2602', '&#9730;'], 'perceive': [u'\u25f5', '&#9717;'], 'mac': [u'\u2318', '&#8984;'], 'eject': [u'\u23cf', '&#9167;'], 'medical': [u'\u2624', '&#9764;'], 'biohazard': [u'\u2623', '&#9763;'], 'blackdiamond': [u'\u2756', '&#10070;'], 'castle': [u'\u2656', '&#9814;'], 'star': [u'\u2605', '&#9733;'], 'sagit': [u'\u2650', '&#9808;']}
 
 # dictionary to convert ARPA -> IPA
-arpaToIPA = {'IH': u'\u026a', 'UH R': u'\u028ar', 'UH' : u'\u028a', 'UW' : 'u', 'IH': u'\u026a', 'AH': u'\u028c', 'AA R': u'\u0251r', 'AH': u'\u0259', 'AH': u'\u028c', 'JH': u'd\u0292', 'AW R': u'a\u028ar', 'EH': u'\u025b', 'EH': u'\u025b', 'EH': u'\u025b', 'EY': u'e\u026a', 'AO R': u'\u0254r', 'EY': u'e\u026a', 'EH R': u'\u025br', 'AY': u'a\u026a', 'IH': u'\u026a', 'AY': u'a\u026a', 'R': u'\u0279', 'D': u'd', 'AO R': u'\u0254r', 'AW': u'a\u028a', 'AW': u'a\u028a', 'AW': u'a\u028a', 'P': u'p', 'AO': u'\u0254', 'AX': u'\u028c', 'IH R': u'\u026ar', 'IY R': u'\u026ar', 'OY': u'\u0254\u026a', 'UH R': u'\u028ar', 'UW': u'u', 'HH': u'h', 'UH': u'\u028a', 'EH R': u'\u025br', 'AO R': u'\u0254r', 'ZH': u'\u0292', 'G': u'\u0261', 'K': u'k', 'S': u's', 'IH R': u'\u026ar', 'W': u'w', 'ER': u'\u025d', 'AE': u'\xe6', 'AE': u'\xe6', 'IH R': u'\u026ar', 'AW R': u'a\u028ar', 'EY': u'e\u026a', 'EH R': u'\u025br', 'NG': u'\u014b', 'CH': u't\u0283', 'F': u'f', 'N': u'n', 'R': u'\u0279', 'V': u'v', 'Z': u'z', 'MEYE': u'm\xe6', 'AY': u'a\u026a', 'SH': u'\u0283', 'AW R': u'a\u028ar', 'DH': u'\xf0', 'IY R': u'\u026ar', 'B': u'b', 'DX': u'\u027e', 'AA R': u'\u0251r', 'TH': u'\u03b8', 'IY': u'i', 'M': u'm', 'L': u'l', 'IH': u'\u026a', 'IY R': u'\u026ar', 'Y': u'j', 'OW': u'o\u028a', 'ER': u'\u025d', 'IY': u'i', 'AA': u'\u0251', 'T': u't'}
+arpaToIPA = {'IH': u'\u026a', 'UH R': u'\u028ar', 'ER' : u'\u025d', 'UH' : u'\u028a', 'UW' : 'u', 'IH': u'\u026a', 'AH': u'\u028c', 'AA R': u'\u0251r', 'AH': u'\u0259', 'AH': u'\u028c', 'JH': u'd\u0292', 'AW R': u'a\u028ar', 'EH': u'\u025b', 'EH': u'\u025b', 'EH': u'\u025b', 'EY': u'e\u026a', 'AO R': u'\u0254r', 'EY': u'e\u026a', 'EH R': u'\u025br', 'AY': u'a\u026a', 'IH': u'\u026a', 'AY': u'a\u026a', 'R': u'\u0279', 'D': u'd', 'AO R': u'\u0254r', 'AW': u'a\u028a', 'AW': u'a\u028a', 'AW': u'a\u028a', 'P': u'p', 'AO': u'\u0254', 'AX': u'\u028c', 'IH R': u'\u026ar', 'IY R': u'\u026ar', 'OY': u'\u0254\u026a', 'UH R': u'\u028ar', 'UW': u'u', 'HH': u'h', 'UH': u'\u028a', 'EH R': u'\u025br', 'AO R': u'\u0254r', 'ZH': u'\u0292', 'G': u'\u0261', 'K': u'k', 'S': u's', 'IH R': u'\u026ar', 'W': u'w', 'AE': u'\xe6', 'AE': u'\xe6', 'IH R': u'\u026ar', 'AW R': u'a\u028ar', 'EY': u'e\u026a', 'NG': u'\u014b', 'CH': u't\u0283', 'F': u'f', 'N': u'n', 'R': u'\u0279', 'V': u'v', 'Z': u'z', 'MEYE': u'm\xe6', 'AY': u'a\u026a', 'SH': u'\u0283', 'AW R': u'a\u028ar', 'DH': u'\xf0', 'IY R': u'\u026ar', 'B': u'b', 'DX': u'\u027e', 'AA R': u'\u0251r', 'TH': u'\u03b8', 'IY': u'i', 'M': u'm', 'L': u'l', 'IH': u'\u026a', 'IY R': u'\u026ar', 'Y': u'j', 'OW': u'o\u028a', 'ER': u'\u025d', 'IY': u'i', 'AA': u'\u0251', 'T': u't'}
 
 # dictionary to convert IPA -> ARPA
-ipaToARPA = {u'\u0283': ['SH'], u't\u0361\u0283' : ['CH0', 'CH1', 'CH2', 'CH'], u'd\u0361\u0292' : ['JH'], 'u' : 'UW', u'a\u028a': ['AW1', 'AW0', 'AW2'], u'o\u028a': ['OW1', 'OW0', 'OW2'], u'e\u026a': ['EY1', 'EY0', 'EY2'], u'a\u028ar': ['AW0 R', 'AW2 R', 'AW1 R'], u'a\u026a': ['AY1', 'AY0', 'AY2'], u'\u0254r': ['AO1 R', 'AO0 R', 'AO2 R'], u'\u028c': ['AH2', 'AH1', 'AX0'], u'\u0292': ['ZH'], u'\u0254\u026a': ['OY2', 'OY1', 'OY0'], u'\u025br': ['EH0 R', 'EH1 R', 'EH2 R'], u'\xf0': ['DH'], u'j': ['Y'], u'\u03b8': ['TH'], u'b': ['B'], u'd\u0292': ['JH'], u'\u0251r': ['AA0 R', 'AA1 R', 'AA2 R'], u'\u014b': ['NG'], u'\u026ar': ['IH0 R', 'IY0 R', 'IH2 R', 'IH1 R', 'IY1 R', 'IY2 R'], u'\u0251': ['AA0', 'AA2', 'AA1'], u'm\xe6': ['MEYE1'], u'\u0254': ['AO2', 'AO1', 'AO0'], u'\u0259': ['AH0'], u'\u025b': ['EH2', 'EH0', 'EH1'], u'\u025a': ['ER1', 'ER2'], u'\u025d': ['ER', 'ER0'], u't\u0283': ['CH'], u'\u0261': ['G'], u'f': ['F'], u'd': ['D'], u'\xe6': ['AE1', 'AE0', 'AE2'], u'i': ['IY1', 'IY0', 'IY2'], u'h': ['HH'], u'k': ['K'], u'\u026a': ['IH2', 'IH2', 'IH1', 'IH0', 'IH'], u'm': ['M'], u'l': ['L'], u'\u028ar': ['UH0 R', 'UH0 R', 'UH2 R', 'UH1 R'], u'n': ['N'], u'r': ['R'], u'\u028a': ['UH2', 'UH0', 'UH1'], u'p': ['P'], u's': ['S'], u'u': ['UW2', 'UW1', 'UW0'], u't': ['T'], u'w': ['W'], u'v': ['V'], u'\u0279': ['R0', 'R'], u'z': ['Z'], u'\u027e': ['DX']}
+ipaToARPA = {u'\u0283': 'SH', u'\u025d' : 'ER', u't\u0361\u0283' : 'CH', u'd\u0361\u0292' : 'JH', 'u' : 'UW', u'a\u028a': 'AW', u'o\u028a': 'OW', u'e\u026a': 'EY', u'a\u028ar': 'AW R', u'a\u026a': 'AY', u'\u0254r': 'AO R', u'\u028c': 'AH', u'\u0292': 'ZH', u'\u0254\u026a': 'OY2', u'\u025br': 'EH R', u'\xf0': 'DH', u'j': 'Y', u'\u03b8': 'TH', u'b': 'B', u'd\u0292': 'JH', u'\u0251r': 'AA R',  u'\u014b': 'NG', u'\u026ar': 'IH R', u'\u026ar': 'IH R', u'\u0251': 'AA', u'm\xe6': 'MEYE', u'\u0254': 'AO', u'\u0259': 'AH', u'\u025b': 'EH', u'\u025a': 'ER', u'\u025d': 'ER', u't\u0283': 'CH', u'\u0261': 'G', u'f': 'F', u'd': 'D', u'\xe6': 'AE', u'i': 'IY', u'h': 'HH', u'k': 'K', u'\u026a': 'IH', u'm': 'M', u'l': 'L', u'\u028ar': 'UH R', u'n': 'N', u'r': 'R', u'\u028a': 'UH', u'p': 'P', u's': 'S', u'u': 'UW', u't': 'T', u'w': 'W', u'v': 'V', u'\u0279': 'R', u'z': 'Z', u'\u027e': 'DX'}
 
 # visemic classes as seen in auer and bernstein
 # http://cloudedbox.com/FALT/auerandbernstein.pdf
@@ -136,7 +136,6 @@ class FALT(object):
    		return [symbolized, info[0], info[1], info[2], visemeSet]
 
    	def getSimilarities(self, word, size, maxDistance=1):
-   		#index : { 0 : 1, 1 : 2, 2 : 10, 3 : 12, 4 : 19, 5 : 28 }
    		if word == '':
    			return []
    		similar = []
@@ -168,6 +167,63 @@ class FALT(object):
 			return self.familiarity[word]
 		except:
 			return 0
+	# Custom Fun
+	def customRun(self, words = "", classes = "", distance = 1, ipa = False):
+		#parse words
+		words = words.split(",")
+		#parse classes
+		classes = classes.split("|")
+		newClasses = {}
+		phonToSym = {}
+		#parse groups & symbols
+		for i, visemeClass in enumerate(classes):
+			num = i+1
+			group = visemeClass.split(",")
+			newClasses[num] = []
+			for phoneme in group:
+				if ipa:
+					try:
+						translated = ipaToARPA(phoneme)
+						newClasses[num].append(translated)
+						newClasses[phoneme] = str(num)
+						phonToSym[translated] = symbols[symbols.keys()[num]][0]
+					except:
+						newClasses[num].append('?')
+						newClasses[phoneme] = '?'
+				else:
+					phoneme = re.sub(r'[0-9]', '', phoneme)
+					newClasses[num].append(phoneme)
+					newClasses[phoneme] = str(num)
+					phonToSym[phoneme] = symbols[symbols.keys()[num]][0]
+		#get dictionary
+		dictionary = {}
+		for key in self.phonemes.keys():
+			dictionary[key] = []
+			part1 = re.sub(r'[0-9]', '', self.phonemes[key][0])
+			part2 = self.phonemes[key][1]
+			part3 = []
+			part4 = []
+			for phon in part1.split(" "):
+				try:
+					part3.append(phonToSym[phon])
+					part4.append(newClasses[phon])
+				except:
+					part3.append("?")
+					part4.append("?")
+			dictionary[key].append([part1, part2, part3, part4])
+		#symbolize word
+		print dictionary['WORLD']
+		symbolized = {}
+		for word in words:
+			word = word.upper()
+			symbolized[word] = []
+			try:
+				symbolized[word].append(' '.join(dictionary[word][0][2]))
+				symbolized[word].append(' '.join(dictionary[word][0][3]))
+			except:
+				symbolized[word].append("Not Found") 
+				symbolized[word].append("")
+		return symbolized
 def main():
 	return
 
